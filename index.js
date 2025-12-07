e56a7ebfb97f4c76f9ae53020c30c3f791a604e3
Fahim <ashfahim18@gmail.com>
2025-12-07 23:14:01 +0600

    .env file added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const app = express()
 const port = 4000
 
@@ -9,7 +10,7 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://PawMart:ndeHZ1RbcDi5RS4u@cluster0.konzx.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.konzx.mongodb.net/?appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
